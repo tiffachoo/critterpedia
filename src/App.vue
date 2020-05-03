cc4db9e889f1fa43c767e6e45b36f02d0f137774
Tiffany Choong <tiff.a.choong@gmail.com>
2020-05-03 19:15:52 -0400

    🚿 Fix formatting

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <div id="app">
+	<div id="app">
 		<router-view />
-  </div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
-  }
+	name: 'App',
+	components: {
+	}
 }
 </script>
 
