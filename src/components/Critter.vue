3f59a132677c86e619028d92c7dca37909b0b923
Tiffany Choong <tiff.a.choong@gmail.com>
2020-05-03 21:52:46 -0400

    🛠 Plop in fish data and add critter component; add base styles

---
+++ src/components/Critter.vue
@@ -0,0 +1,46 @@
+<template>
+	<li class="critter">
+		<img 
+			:alt="`Thumbnail of ${name}`"
+			class="critter-image"
+			src="" 
+		>
+		<span class="critter-text">
+			{{ name }}
+		</span>
+	</li>
+</template>
+
+<script>
+export default {
+	name: 'Critter',
+	props: {
+		name: String
+	}
+}
+</script>
+
+<style lang="scss">
+.critter {
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	padding: var(--spacer);
+	background-color: var(--primary-color);
+	text-align: center;
+
+	&-image {
+		display: inline-block;
+		width: 100%;
+		max-width: 6rem;
+		margin-bottom: var(--spacer-sm);
+		border-radius: 100%;
+		background-color: var(--primary-color-tint);
+	}
+
+	&-text {
+		font-size: 0.875rem;
+	}
+}
+</style>
--- src/components/CritterList.vue
+++ src/components/CritterList.vue
@@ -1,19 +1,45 @@
 <template>
-	<div>
+	<div class="critter-list">
 		<h1>
 			Critters
 		</h1>
+		<!-- <input type="radio"> -->
+		<ul class="critter-list-content">
+			<critter
+				v-for="critter in critters"
+				:key="critter.id"
+				:name="critter.name"
+			/>
+		</ul>
+		<!-- <table></table> -->
 	</div>
 </template>
 
 <script>
+import Critter from './Critter.vue'
+import { data } from '../data/fish.json';
+
 export default {
-	
+	name: 'CritterList',
+	components: {
+		Critter,
+	},
+	data() {
+		return {
+			critters: data
+		}
+	}
 }
 </script>
 
 <style lang="scss">
-h1 {
-	color: $primary-color;
+.critter-list {
+	flex: 0 0 100%; 
+
+	&-content {
+		display: grid;
+		grid-gap: var(--spacer);
+		grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+	}
 }
 </style>
