f2cf8ee4a66ffe92749411f72b6f7699c362cefc
Tiffany Choong <tiff.a.choong@gmail.com>
2020-05-17 22:06:38 -0400

    🎨 Add hover styles to critter; adjust image clipping

--- src/components/Critter.vue
+++ src/components/Critter.vue
@@ -1,9 +1,11 @@
 <template>
 	<li class="critter">
-		<div 
-			class="critter-image"
-			:style="{ backgroundImage: `url('${image}')`, backgroundPosition }"
-		/>
+		<div class="critter-image-wrap">
+			<div 
+				:style="{ backgroundImage: `url('${image}')`, backgroundPosition }"
+				class="critter-image" 
+			/>
+		</div>
 		<span class="critter-text">
 			{{ name }}
 		</span>
@@ -40,16 +42,48 @@ export default {
 
 	&-image {
 		display: inline-block;
-		height: 6rem;
-		width: 6rem;
-		margin-bottom: var(--spacer-sm);
-		border-radius: 100%;
-		background-color: var(--primary-color);
+		height: 100%;
+		width: 100%;
 		background-size: 1000%;
+		transition: transform 0.2s ease-in-out;
+
+		&-wrap {
+			position: relative;
+			z-index: 1;
+			height: 6rem;
+			width: 6rem;
+			margin-bottom: var(--spacer-sm);
+
+			&::before {
+				content: '';
+				position: absolute;
+				z-index: -1;
+				top: 10%;
+				left: 10%;
+				display: block;
+				height: 80%;
+				width: 80%;
+				border-radius: 100%;
+				background-color: var(--primary-color);
+				transition: transform 0.3s ease-in-out;
+			}
+		}
 	}
 
 	&-text {
 		font-size: 0.875rem;
+	}
+
+	&:hover {
+		.critter-image {
+			transform: scale(1.1);
+
+			&-wrap::before {
+				@include diagPattern;
+
+				transform: scale(1.2);
+			}
+		}
 	}
 }
 </style>
