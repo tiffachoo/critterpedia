7bdb4885da31471931d6f5186c9bf6cf35ef8da3
Tiffany Choong <tiff.a.choong@gmail.com>
2020-05-30 22:33:45 -0400

    🎨 Wip, add little sparky lines to button

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -5,6 +5,12 @@
 		:class="{ 'button-circle': circle }"
 		class="button"
 	>
+		<svg class="button-sparks button-sparks-left">
+			<line class="button-sparks-line button-sparks-line-top" x1="5" y1="2.5" x2="15" y2="2.5" />
+			<line class="button-sparks-line button-sparks-line-middle" x1="5" y1="50%" x2="15" y2="50%" />
+			<line class="button-sparks-line button-sparks-line-bottom" x1="5" y1="100%" x2="15" y2="100%" />
+		</svg>
+
 		<slot />
 	</component>
 </template>
@@ -39,6 +45,37 @@ export default {
 	color: var(--button-color);
 	text-decoration: none;
 
+	&-sparks {
+		position: absolute;
+		top: 0;
+		height: 100%;
+		width: 30px;
+
+		&-line {
+			fill: none;
+			stroke: var(--button-border-color);
+			stroke-width: 5;
+			stroke-linecap: round;
+			stroke-miterlimit: 10;
+			stroke-dasharray: 8 22;
+			stroke-dashoffset: 15;
+
+			&-top {
+				transform: translate(10px, 0) rotate(45deg);
+				transform-origin: 0 2.5px;
+			}
+
+			&-bottom {
+				transform: translate(10px, -2.5px) rotate(-45deg);
+				transform-origin: 0 100%;
+			}
+		}
+
+		&-left {
+			left: -1.25rem;
+		}
+	}
+
 	&-circle {
 		display: inline-grid;
 		place-items: center;
@@ -52,6 +89,12 @@ export default {
 		}
 	}
 
+	&:active {
+		.button-sparks-line {
+			animation: plink 0.6s cubic-bezier(0.8, 0, 0.2, 0.9) forwards;
+		}
+	}
+
 	&.active {
 		--button-background-color: var(--primary-color-darkest);
 
@@ -59,4 +102,13 @@ export default {
 	}
 }
 
+@keyframes plink {
+	0% { stroke-dashoffset: -15; }
+	50%{ stroke-dashoffset: 0; }
+	70% { opacity: 1; }
+	100% { 
+		stroke-dashoffset: 15; 
+		opacity: 0; 
+	}
+}
 </style>
